7afc88bc907a4a140a0b1df3782f57beb82c61af
ShakeMartini <chrisanzalone@att.net>
2015-11-30 16:19:53 -0600

    Test Framework Added

---
+++ public/qa/tests-about.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+suite('"About" Page Tests', function () {
+    test('page should contain link to contact page', function () {
+        assert($('a[href="/contact"]').length);
+    });
+});
---
+++ public/qa/tests-global.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+suite('Global Tests', function () {
+    test('page has a valid title', function () {
+        assert(document.title && document.title.match(/\S/) &&
+            document.title.toUpperCase() !== 'TODO');
+    });
+});
