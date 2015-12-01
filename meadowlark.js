0c9805a76e7119788c30acc4e9e3431a3f4f1d71
ShakeMartini <chrisanzalone@att.net>
2015-11-30 19:31:31 -0600

    Various QA Exercises

--- meadowlark.js
+++ meadowlark.js
@@ -13,11 +13,15 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.disable('x-powered-by');
+
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
 });
+
+// routes
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -28,6 +32,27 @@ app.get('/about', function (req, res) {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
     });
+});
+
+// view response header information
+app.get('/headers', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) s += name + ':' + req.headers[name] + '\n';
+    res.send(s);
+});
+
+
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function (req, res) {
+    res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
 });
 
 // 404 catch-all handler (middleware)
